fdc2b57252e1fc33a2525fe97f6766026e6d5f06
André Willrich <andre@willrich.info>
2021-06-09 09:13:52 +0200

    Routes Tests, TestStations, TestManufacturers

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\TestController;
+use App\Http\Controllers\TestManufacturerController;
+use App\Http\Controllers\TestStationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+#Route::middleware('auth:api')->apiResources([
+Route::apiResources([
+    'tests', TestController::class,
+    'test_stations', TestStationController::class,
+    'test_manufacturer', TestManufacturerController::class,
+    ]);
+
